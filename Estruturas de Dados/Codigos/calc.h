b22dd8d2637cbe873eaaff95151653682b124434
Ernesto Alves <98620218+Ernesto-Alves67@users.noreply.github.com>
2024-02-25 10:17:06 -0300

    Update calc.h
    adicionando definição de mais funcionalidades.

--- Estruturas de Dados/Codigos/calc.h
+++ Estruturas de Dados/Codigos/calc.h
@@ -6,4 +6,18 @@ void integral(int a);
 void esfera(int b);
 void identForm(int b);
 
+
+void derivada_parcial(int argumento, int var_derivacao);
+void gradiente(int funcao);
+void ponto_minimo(int funcao);
+void ponto_maximo(int funcao);
+void integral_dupla(int funcao, int lim_a, int lim_b, int lim_c, int lim_d);
+void integral_triple(int funcao);
+
+
+void jacobiano(int funcao);
+void laplace_function();
+
+
+
 #endif
